fe35e97f28acc7e72706af7ce2f53e0c6204dfcd
lgf228 <35672324+lgf228@users.noreply.github.com>
2026-02-02 00:26:18 +0800

    refactor: 统一入口到src/，归档冗余文档，删除重复配置

--- pages/index.ts
+++
@@ -1,7 +0,0 @@
-// pages/index.ts
-// 页面组件入口文件
-
-// 导出页面组件
-export { default as HomePage } from './HomePage.vue'
-export { default as SparkDemoPage } from './SparkDemoPage.vue'
-export { default as EJ2DemoPage } from './EJ2DemoPage.vue'
